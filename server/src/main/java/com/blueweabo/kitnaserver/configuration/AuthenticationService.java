b3a84f4f7dbf1dc41233b1506080e5193bc4622a
BlueWeabo <ilia.iliev2005@gmail.com>
2024-05-14 12:06:13 +0300

    update mobule to use react native

--- server/src/main/java/com/blueweabo/kitnaserver/configuration/AuthenticationService.java
+++ server/src/main/java/com/blueweabo/kitnaserver/configuration/AuthenticationService.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class AuthenticationService {
 
     private static final String AUTH_TOKEN_HEADER_NAME = "KG-API-KEY";
-    private static final UUID AUTH_TOKEN = UUID.fromString("");
+    private static final UUID AUTH_TOKEN = UUID.fromString("12b6bcf1-c8fe-43c4-b01f-6684129e8bdf");
 
     public static Authentication getAuthentication(HttpServletRequest request) {
         String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
